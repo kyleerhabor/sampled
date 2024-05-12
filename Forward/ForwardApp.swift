849dc4fb46f36828849d741e2450743e537b7628
Kyle Erhabor <kyleerhabor@gmail.com>
2024-05-12 18:29:31 -0400

    Initial Commit

---
+++ Forward/ForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForwardApp.swift
+//  Forward
+//
+//  Created by Kyle Erhabor on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
