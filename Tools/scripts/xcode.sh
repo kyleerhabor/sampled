0a8b902f4bf6e6a2769eb82cb4669ba6fe0adfc2
Kyle Erhabor <kyleerhabor@gmail.com>
2025-10-31 20:55:32 -0400

    Add Homebrew to build path

--- Tools/scripts/xcode.sh
+++ Tools/scripts/xcode.sh
@@ -6,6 +6,9 @@
 #  Created by Kyle Erhabor on 11/8/24.
 #
 
+# We're using sh, so we don't have whatever Zsh or Bash sources.
+export PATH="$(env brew --prefix)/bin:$PATH"
+
 if [ "$CONFIGURATION" = "Release" ]; then
   export EXTRA_CFLAGS="-O3"
   export EXTRA_FFMPEGFLAGS=(--disable-programs --disable-doc)
