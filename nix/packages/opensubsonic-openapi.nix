37834b0e21d81463b1a661d29657dedfd69a8651
Kyle Erhabor <kyleerhabor@gmail.com>
2026-06-28 01:09:59 -0400

    Make OpenSubsonic API npm package reproducible

--- nix/packages/opensubsonic-openapi.nix
+++ nix/packages/opensubsonic-openapi.nix
@@ -1,11 +1,19 @@
-{ fetchNpmPackage, fetchFromGitHub }: fetchNpmPackage {
+{ buildNpmPackage, fetchFromGitHub, cacert }: buildNpmPackage {
   name = "opensubsonic-openapi";
   strictDeps = true;
   src = fetchFromGitHub {
-    owner = "opensubsonic";
+    owner = "kyleerhabor";
     repo = "open-subsonic-api";
-    rev = "7eed79524b218236d81a975d13c4ba29b4ea1d7e";
-    hash = "sha256-IoDEoh58OqiixLu8n3N/G9Fzqm4WYoTuGZLTLGw7XfM=";
+    rev = "12f557ebdf195bf429d52134832ae8a87f02b754";
+    hash = "sha256-dABzOQb2uoCBQreMUtNQQIPLbwJl38xgquimRjaRBzU=";
   };
+  nativeBuildInputs = [cacert];
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  npmDepsHash = "sha256-d7GDnywwJgIRF3lpgrD9MQlGq0+50oRZZyY/csJjIMM=";
   npmBuildScript = "build:openapi";
+  installPhase = ''
+    runHook preInstall
+    cp content/en/docs/Openapi/openapi.json $out
+    runHook postInstall
+  '';
 }
